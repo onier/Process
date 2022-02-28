05b81f8d1e87cd730787416194a7f5c3716fee2d
xuzhenhai <christ_2014@foxmail.com>
2022-02-28 19:52:52 +0800

    add subtask event example

--- TaskImpl/src/ExceptionTask.cpp
+++ TaskImpl/src/ExceptionTask.cpp
@@ -12,14 +12,14 @@ bool ExceptionTask::initTask(Process::ProcessContext* manager) {
 }
 
 void ExceptionTask::run(std::shared_ptr<Process::ProcessContext> manager) {
-    if (_runTimeExceptionCounter < 5) {
-        _runTimeExceptionCounter++;
-        LOG(INFO) << "_runTimeExceptionCounter is " << _runTimeExceptionCounter;
-        manager->notifyEvent("EventA",this);
-        throw Process::TaskRuntimeException("", "retry again");
-    } else {
-        _runTimeExceptionCounter = 1;
-    }
+//    if (_runTimeExceptionCounter < 5) {
+//        _runTimeExceptionCounter++;
+//        LOG(INFO) << "_runTimeExceptionCounter is " << _runTimeExceptionCounter;
+//        manager->notifyEvent("EventA",this);
+//        throw Process::TaskRuntimeException("", "retry again");
+//    } else {
+//        _runTimeExceptionCounter = 1;
+//    }
     manager->notifyEvent("EventB",this);
-    throw Process::TaskError("reset process");
+//    throw Process::TaskError("reset process");
 }
